63862d070aaed2af42af6c691334b34501112b47
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-10-03 12:17:05 -0700

    Binded Cells

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,7 @@ angular.module('newTicApp', [])
   });
 
 function GameCtrl($scope) {
-  $scope.gamex = [['hi1', 'hi2', 'hi3'], ['hi4', 'hi5', 'hi6'], ['hi7', 'hi8', 'hi9']];
+  $scope.gamex = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 
   $scope.bttns = [{
     label: "Start Game ›",
@@ -26,16 +26,60 @@ function GameCtrl($scope) {
     id: 'playr2'
   }];
 
-    // Switch views between hidden and visible elements
-    var section = 1;
-    
-    $scope.section = function (id) {
-        section = id;   
-    };
-    
-    $scope.is = function (id) {
-        return section == id;
-    };
+  $scope.playCell = function(col, row){
+    $scope.gamex[col][row] = 'X';
+  }
+
+  $scope.navCells = function(){
+    for(c=0;c<=2;++c){
+      for(r=0;r<=2;++r){
+        cellArray[c][r] = document.getElementById("cell"+c+"_"+r).innerHTML;
+      }
+    }
+    for(x=0; x<=2; ++x){
+      if(cellArray[0][x] == cellArray[1][x] && cellArray[1][x] == cellArray[2][x] && cellArray[0][x] != "&nbsp;"){
+        alreadyWon = true;
+        alert(cellArray[0][x] + " WON!");
+        toggleVisibility('playagainbtn');
+      }
+      if(cellArray[x][0] == cellArray[x][1] && cellArray[x][1] == cellArray[x][2] && cellArray[x][0] != "&nbsp;"){
+        alreadyWon = true;
+        alert(cellArray[x][0] + " WON!");
+        toggleVisibility('playagainbtn');
+      }
+    }
+    if(cellArray[0][0] == cellArray[1][1] && cellArray[1][1] == cellArray[2][2] && cellArray[0][0] != "&nbsp;" ){
+      alreadyWon = true;
+      alert(cellArray[0][0] + " WON!");
+      toggleVisibility('playagainbtn');
+    }
+    if(cellArray[2][0] == cellArray[1][1] && cellArray[1][1] == cellArray[0][2] && cellArray[2][0] != "&nbsp;" ){
+      alreadyWon = true;
+      alert(cellArray[2][0] + " WON!");
+      toggleVisibility('playagainbtn');
+    }
+    else if(playedCells == 9 && alreadyWon != true){
+      alert("No one won.");
+      toggleVisibility('playagainbtn');
+    }
+  }
+  
+
+
+
+
+
+
+  // Switch views between hidden and visible elements
+  var section = 1;
+  
+  $scope.section = function (id) {
+      section = id;   
+  };
+  
+  $scope.is = function (id) {
+      return section == id;
+  };
 }
 
 
