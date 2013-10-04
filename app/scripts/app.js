1f47e005200c35cd16c6a05773cebaa2916c554f
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-10-03 22:25:56 -0700

    Gettingng Closer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,11 +27,21 @@ function GameCtrl($scope) {
   }, {
     label: "Play Again ›",
     id: 'playagainbtn'
+  }, {
+    label: "Helvetica Neue not Installed. Try Again?",
+    id: 'fontdetect'
   }];
 
   var turn = 1;
-  var playedCells = 0;
   var alreadyWon = false;
+
+// Font Detection for Helvetica Neue
+$scope.detectHelv = function(){
+  var d = new Detector();
+  if(d.detect('Helvetica Neue') == false){
+    this.section(5);
+  }
+}
 
   // Setting Player as X or O
   $scope.playBall = function(numPlayr){
@@ -42,45 +52,74 @@ function GameCtrl($scope) {
     if (alreadyWon == false && this.gamex[row][col] != 'X' && this.gamex[row][col] != 'O') {
       if (turn % 2 != 0) {
         turn++;
-        playedCells++;
         this.gamex[row][col] = 'X';
       }
       else {
         turn++;
-        playedCells++;
         this.gamex[row][col] = 'O';
       }
-  if(this.gamex[0][col] == this.gamex[1][col] && this.gamex[1][col] == this.gamex[2][col] && this.gamex[0][col] != null){
-    alreadyWon = true;
-    alert(this.gamex[0][col] + " WON!");
-    this.section(4);
-  }
-  if(this.gamex[row][0] == this.gamex[row][1] && this.gamex[row][1] == this.gamex[row][2] && this.gamex[row][0] != null){
-    alreadyWon = true;
-    alert(this.gamex[x][0] + " WON!");
-    this.section(4);
-  }
-  if(this.gamex[0][0] == this.gamex[1][1] && this.gamex[1][1] == this.gamex[2][2] && this.gamex[0][0] != null ){
-    alreadyWon = true;
-    alert(this.gamex[0][0] + " WON!");
-    this.section(4);
-  }
-  if(this.gamex[2][0] == this.gamex[1][1] && this.gamex[1][1] == this.gamex[0][2] && this.gamex[2][0] != null ){
-    alreadyWon = true;
-    alert(this.gamex[2][0] + " WON!");
-    this.section(4);
-  }
-  else if(playedCells == 9 && alreadyWon != true){
-    alert("No one won.");
-    this.section(4);
-  }
-
-
-
+      // Win Conditions
+      if(this.gamex[0][col] == this.gamex[1][col] && this.gamex[1][col] == this.gamex[2][col] && this.gamex[0][col] != null){
+        alreadyWon = true;
+        alert(this.gamex[0][col] + " WON!");
+        this.section(4);
+      }
+      if(this.gamex[row][0] == this.gamex[row][1] && this.gamex[row][1] == this.gamex[row][2] && this.gamex[row][0] != null){
+        alreadyWon = true;
+        alert(this.gamex[row][0] + " WON!");
+        this.section(4);
+      }
+      if(this.gamex[0][0] == this.gamex[1][1] && this.gamex[1][1] == this.gamex[2][2] && this.gamex[0][0] != null ){
+        alreadyWon = true;
+        alert(this.gamex[0][0] + " WON!");
+        this.section(4);
+      }
+      if(this.gamex[2][0] == this.gamex[1][1] && this.gamex[1][1] == this.gamex[0][2] && this.gamex[2][0] != null ){
+        alreadyWon = true;
+        alert(this.gamex[2][0] + " WON!");
+        this.section(4);
+      }
+      else if(alreadyWon == false && this.gamex[row][col] != 'X' && this.gamex[row][col] != 'O'){
+        alert("No one won.");
+        this.section(4);
+      }
 
     }
     else {
       console.log('Nice try, punk');
+    }
+    // Shake to Reload on iPhone
+    if (typeof window.DeviceMotionEvent != 'undefined') {
+        // Shake sensitivity (a lower number is more)
+        var sensitivity = 50;
+
+        // Position variables
+        var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+
+        // Listen to motion events and update the position
+        window.addEventListener('devicemotion', function (e) {
+            x1 = e.accelerationIncludingGravity.x;
+            y1 = e.accelerationIncludingGravity.y;
+            z1 = e.accelerationIncludingGravity.z;
+        }, false);
+
+        // Periodically check the position and fire
+        // if the change is greater than the sensitivity
+        setInterval(function () {
+            var change = Math.abs(x1-x2+y1-y2+z1-z2);
+
+            if (change > sensitivity) {
+                var shakeGame = confirm("Do you want to start a new game?");
+          if (shakeGame == true){
+            window.location.reload();
+          }
+            }
+
+            // Update new position
+            x2 = x1;
+            y2 = y1;
+            z2 = z1;
+        }, 150);
     }
 }
 
@@ -107,5 +146,8 @@ function GameCtrl($scope) {
       this.gamex[a][b] = null;
       };
     }
+    this.section(2);
+    this.turn = 0;
+    this.alreadyWon = false;
   }
 }
