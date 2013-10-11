33e4b2cccba44e08806ef7cd929628347f6bdeff
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-10-11 15:40:46 -0700

    Ugh

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,35 +14,6 @@ angular.module('newTicApp', ["firebase"])
 
 function GameCtrl($scope, angularFire) {
 
-// Choose stylesheet
-var rand_style_no = Math.ceil(5 * Math.random());
-
-jQuery(function($){
-
-$.supersized({
-  slides  :   [ {image : 'img/'+rand_style_no+'bg.jpg', title : ''} ]
-});
-});
-
-switch(rand_style_no)
-{
-  case 1:
-    document.write('<!-- build:css(.tmp) styles/1.css --><link rel="stylesheet" href="styles/1.css" type="text/css"><!-- endbuild -->');
-    break;
-  case 2:
-    document.write('<!-- build:css(.tmp) styles/2.css --><link rel="stylesheet" href="styles/2.css" type="text/css"><!-- endbuild -->');
-    break;
-  case 3:
-    document.write('<!-- build:css(.tmp) styles/3.css --><link rel="stylesheet" href="styles/3.css" type="text/css"><!-- endbuild -->');
-    break;
-  case 4:
-    document.write('<!-- build:css(.tmp) styles/4.css --><link rel="stylesheet" href="styles/4.css" type="text/css"><!-- endbuild -->');
-    break;
-  default:
-    document.write('<!-- build:css(.tmp) styles/main.css --><link rel="stylesheet" href="styles/main.css" type="text/css"><!-- endbuild -->');
-    break;
-}
-
 // Font Detection for Helvetica Neue
 // $scope.detectHelv = function(){
 //   var d = new Detector();
