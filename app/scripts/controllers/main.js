e8b41108de39590a74d4b822a64bc0745a19da36
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-10-11 16:34:13 -0700

    Updated controller method

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,139 +1,172 @@
 'use strict';
-var turn = 1;
-var playedCells = 0;
-var alreadyWon = false;
+
+angular.module("newTicApp")
+  .controller("GameCtrl", ['$scope', function($scope, angularFire){
 
 // Font Detection for Helvetica Neue
-function detectHelv(){
-	var d = new Detector();
-	if(d.detect('Helvetica Neue') == false){
-		toggleDisplay('startgame');
-		toggleDisplay('startgame');
-		toggleDisplay('fontdetect');
-	}
-}
-
-// Setting Player as X or O
-function playBall(numPlayr){
-	turn = numPlayr;
-}
-
-// Shake to Reload on iPhone
-if (typeof window.DeviceMotionEvent != 'undefined') {
-    // Shake sensitivity (a lower number is more)
-    var sensitivity = 50;
-
-    // Position variables
-    var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
-
-    // Listen to motion events and update the position
-    window.addEventListener('devicemotion', function (e) {
-        x1 = e.accelerationIncludingGravity.x;
-        y1 = e.accelerationIncludingGravity.y;
-        z1 = e.accelerationIncludingGravity.z;
-    }, false);
-
-    // Periodically check the position and fire
-    // if the change is greater than the sensitivity
-    setInterval(function () {
-        var change = Math.abs(x1-x2+y1-y2+z1-z2);
-
-        if (change > sensitivity) {
-            var shakeGame = confirm("Do you want to start a new game?");
-			if (shakeGame == true){
-				window.location.reload();
-			}
-        }
-
-        // Update new position
-        x2 = x1;
-        y2 = y1;
-        z2 = z1;
-    }, 150);
-}
-
-// Fill Cells with X or O
-function playIt(){
-	if (alreadyWon == false && event.target.className.split(" ")[3] != "played") {
-		if (turn % 2 != 0) {
-			event.target.classList.add('played');
-			playedCells++;
-			event.target.innerHTML = "X";
-		}
-		else {
-			event.target.classList.add('played');
-			playedCells++;
-			event.target.innerHTML = "O";
-		}
-		turn++;
-		navCells();
-	}
-	else {
-		console.log('Nice try, punk');
-	}
-}
-
-// Check for Win Conditions
-function navCells(){
-	for(var x=0; x<=2; ++x){
-		if(cellArray[0][x] == cellArray[1][x] && cellArray[1][x] == cellArray[2][x] && cellArray[0][x] != "&nbsp;"){
-			alreadyWon = true;
-			alert(cellArray[0][x] + " WON!");
-			toggleVisibility('playagainbtn');
-		}
-		if(cellArray[x][0] == cellArray[x][1] && cellArray[x][1] == cellArray[x][2] && cellArray[x][0] != "&nbsp;"){
-			alreadyWon = true;
-			alert(cellArray[x][0] + " WON!");
-			toggleVisibility('playagainbtn');
-		}
-	}
-	if(cellArray[0][0] == cellArray[1][1] && cellArray[1][1] == cellArray[2][2] && cellArray[0][0] != "&nbsp;" ){
-		alreadyWon = true;
-		alert(cellArray[0][0] + " WON!");
-		toggleVisibility('playagainbtn');
-	}
-	if(cellArray[2][0] == cellArray[1][1] && cellArray[1][1] == cellArray[0][2] && cellArray[2][0] != "&nbsp;" ){
-		alreadyWon = true;
-		alert(cellArray[2][0] + " WON!");
-		toggleVisibility('playagainbtn');
-	}
-	else if(turn == 10 && alreadyWon != true){
-		alert("No one won.");
-		toggleVisibility('playagainbtn');
-	}
-}
-
-// function switchVisibility(id1, id2){
-// 	document.getElementById(id1).style.visibility = 'hidden';
-// 	document.getElementById(id2).style.visibility = 'visible';
+// $scope.detectHelv = function(){
+//   var d = new Detector();
+//   if(d.detect('Helvetica Neue') == false){
+//     this.section(6);
+//   }
 // }
 
-function toggleVisibility(id){
-	var vis = document.getElementById(id).style.visibility;
-	if( vis == 'visible'){
-		document.getElementById(id).style.visibility = 'hidden';
-	}
-	else {
-		document.getElementById(id).style.visibility = 'visible';
-	}
-}
-function switchDisplay(id1, id2){
-	document.getElementById(id1).style.display = 'none';
-	document.getElementById(id2).style.display = 'inline';
-}
-function toggleDisplay(id){
-	var dis = document.getElementById(id).style.display;
-	if( dis == 'inline'){
-		document.getElementById(id).style.display = 'none';
-	}
-	else {
-		document.getElementById(id).style.display = 'inline';
-	}
+// Shake to Reload on iPhone
+// if (typeof window.DeviceMotionEvent != 'undefined') {
+//     // Shake sensitivity (a lower number is more)
+//     var sensitivity = 50;
+
+//     // Position variables
+//     var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
+
+//     // Listen to motion events and update the position
+//     window.addEventListener('devicemotion', function (e) {
+//         x1 = e.accelerationIncludingGravity.x;
+//         y1 = e.accelerationIncludingGravity.y;
+//         z1 = e.accelerationIncludingGravity.z;
+//     }, false);
+
+//     // Periodically check the position and fire
+//     // if the change is greater than the sensitivity
+//     setInterval(function () {
+//         var change = Math.abs(x1-x2+y1-y2+z1-z2);
+
+//         if (change > sensitivity) {
+//             var shakeGame = confirm("Do you want to start a new game?");
+//       if (shakeGame == true){
+//         resetGame();
+//       }
+//         }
+
+//         // Update new position
+//         x2 = x1;
+//         y2 = y1;
+//         z2 = z1;
+//     }, 150);
+// }
+
+  // $scope.$watch('alreadyWon', function(){
+  //   if(this.game[0].alreadyWon == true){
+  //     if(this.cell.value == 'X'){ $scope.game[0].winorlose = 'X WINS!' } else{ $scope.game[0].winorlose = 'O WINS!' };
+  //     this.section(5);
+  //   }
+  //   else if(this.game[0].alreadyWon == false && this.game[0].playedCells == 9){
+  //     $scope.game[0].winorlose = 'NO WIN';
+  //     this.section(5);
+  //   }
+  // });
+
+  $scope.game = [{
+    board: [[{ value: ' '}, { value: ' '}, { value: ' '}],
+            [{ value: ' '}, { value: ' '}, { value: ' '}],
+            [{ value: ' '}, { value: ' '}, { value: ' '}]],
+    alreadyWon: false,
+    turn: 1,
+    playedCells: 0,
+    winorlose: null,
+  }];
+
+  var database = new Firebase("https://tictacneue.firebaseio.com/game");
+  var promise = angularFire(database, $scope, "game");
+
+  promise.then ( function() {
+    $scope.game = [{
+      board: [[{ value: ' '}, { value: ' '}, { value: ' '}],
+              [{ value: ' '}, { value: ' '}, { value: ' '}],
+              [{ value: ' '}, { value: ' '}, { value: ' '}]],
+      alreadyWon: false,
+      turn: 1,
+      playedCells: 0,
+      winorlose: null,
+    }];
+  });
+  var queue = database.child('queue');
+
+  // game[0].player1 = Math.ceil(100 * Math.random());
+  // game[0].player2 = Math.ceil(100 * Math.random());
+  // if(game[0].player2 == game[0].player1){
+  //   game[0].player2++;
+  // };
+
+  $scope.bttns = [{
+    label: "Start Game ›",
+    id: 'startgame'
+  }, {
+    label: "Choose X ›",
+    id: 'playr1'
+  }, {
+    label: "Choose O ›",
+    id: 'playr2'
+  }, {
+    label: "Play Again ›",
+    id: 'playagainbtn'
+  }, {
+    label: "Helvetica Neue not Installed. Try Again?",
+    id: 'fontdetect'
+  }];
+
+  // Setting Player as X or O
+  $scope.playBall = function(numPlayr){
+    this.game[0].turn = numPlayr;
+  }
+
+  $scope.playCell = function(cell){
+    if (this.game[0].alreadyWon == false && this.cell.value != 'X' && this.cell.value != 'O') {
+      if (this.game[0].turn % 2 != 0) {
+        this.game[0].turn++;
+        this.game[0].playedCells++;
+        this.cell.value = 'X';
+      }
+      else {
+        this.game[0].turn++;
+        this.game[0].playedCells++;
+        this.cell.value = 'O';
+      }
+      // Win Conditions
+      for(var x=0; x<=2; ++x){
+        if(this.cell.value == this.game[0].board[0][x].value && this.cell.value == this.game[0].board[1][x].value && this.cell.value == this.game[0].board[2][x].value && this.cell.value != null){
+        this.game[0].alreadyWon = true;
+        }
+        if(this.cell.value == this.game[0].board[x][0].value && this.cell.value == this.game[0].board[x][1].value && this.cell.value == this.game[0].board[x][2].value && this.cell.value != null){
+        this.game[0].alreadyWon = true;
+        }
+      }
+      if(this.cell.value == this.game[0].board[2][0].value && this.cell.value == this.game[0].board[1][1].value && this.cell.value == this.game[0].board[0][2].value && this.cell.value != null ){
+        this.game[0].alreadyWon = true;
+      }
+      if(this.cell.value == this.game[0].board[0][0].value && this.cell.value == this.game[0].board[1][1].value && this.cell.value == this.game[0].board[2][2].value && this.cell.value != null ){
+        this.game[0].alreadyWon = true;
+      }
+    }
+    else {
+      console.log('Nice try, punk');
+    }
 }
 
-// Play Sound on Click
-function EvalSound(soundobj){
-	var thissound=document.getElementById(soundobj);
-	thissound.play(); 
-	setTimeout( function() { window.location.reload(); }, 1600);
-}
+  // Switch views between hidden and visible elements
+  var section = 1;
+  
+  $scope.section = function (id) {
+      section = id;   
+  };
+  
+  $scope.is = function (id) {
+      return section == id;
+  };
+
+// Reset Game
+  $scope.resetGame = function(){
+    // Resets game board
+    for (var a = 0; a <= this.game[0].board.length - 1; a++) {
+      for (var b = 0; b <= this.game[0].board.length - 1; b++) {
+        this.game[0].board[a][b].value = ' ';
+      };
+    };
+
+    this.section(2);
+    this.game[0].turn = 0;
+    this.game[0].playedCells = 0;
+    this.game[0].alreadyWon = false;
+  }
+    
+}]);
